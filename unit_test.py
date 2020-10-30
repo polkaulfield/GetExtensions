b1782684c0d42c59cba0cee0b2341d5211275c99
ekistece <mr.tamuraa@gmail.com>
2020-10-30 15:32:48 +0100

    Delete unit_test.py

--- unit_test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-import extensionmanager
-
-em = extensionmanager.ExtensionManager()
-print(em.list_user_extensions())
-print(em.extension_is_local("dash-to-dock@micxgx.gmail.com"))
